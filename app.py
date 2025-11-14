abfd107c7e7ab356728ac18e930eaca6822bc0e4
yihan0112wang-pixel <yihan0112.wang@gmail.com>
2025-11-14 03:10:13 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-from flask import flask
+from flask import Flask, request, render_template 
+
+app = Flask(__name__)
+
+@app.route("/", methods=["Get","POST"])
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
