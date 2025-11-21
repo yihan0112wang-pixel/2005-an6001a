430b9772ca04dd8317f824b51a93075550ead66e
yihan0112wang-pixel <yihan0112.wang@gmail.com>
2025-11-21 01:55:58 +0000

    1

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/", methods=["Get","POST"])
+@app.route("/", methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
+@app.route("/main", methods=["GET","POST"])
+def main():
+    q = request.form.get("q")
+    print(q)
+    return(render_template("main.html"))
+
 if __name__ == "__main__":
-    app.run()
+    app.run(port=2347)
