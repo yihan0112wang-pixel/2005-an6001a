eaecc79364ebb72fb88e599613c126ae8a345f1b
yihan0112wang-pixel <yihan0112.wang@gmail.com>
2025-11-21 03:10:09 +0000

    1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template 
+import joblib
 
 app = Flask(__name__)
 
@@ -12,5 +13,17 @@ def main():
     print(q)
     return(render_template("main.html"))
 
+@app.route("/dbs", methods=["GET","POST"])
+def dbs():
+    return(render_template("dbs.html"))
+
+@app.route("/dbs_prediction", methods=["GET","POST"])
+def dbs_prediction():
+    q = float(request.form.get("q"))
+    print(q)
+    model = joblib.load("dbs.pkl")
+    r= model.predict([[q]])
+    return(render_template("dbs_prediction.html",r=r))
+
 if __name__ == "__main__":
-    app.run(port=2347)
+    app.run(port=2340)
